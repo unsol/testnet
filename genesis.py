1fc2a1e501ce1f6a8124bf1f6e3e6f6919e034f5
Jared Wasinger <j-wasinger@hotmail.com>
2018-08-26 16:10:51 -0700

    add new option for account balance.  add option to load acct code from file or specify it directly

--- genesis.py
+++ genesis.py
@@ -2,7 +2,7 @@ import argparse, json, sys, binascii
 
 parser = argparse.ArgumentParser(description='Preload a Geth genesis with contract accounts')
 parser.add_argument('precompiles', type=str, nargs='+',
-                            help='a string of the form address,codefile')
+                            help='one or more json objects of the form {address: "" , codefile: "/path/to/file/containing/accountcode", code: "accountcode", balance: ""}')
 parser.add_argument('--genesis', metavar='g', type=str, nargs='?',
                             help='a string of the form address,codefile')
 
@@ -18,17 +18,27 @@ except Exception as e:
   sys.exit(1)
 
 for arg in args.precompiles:
-  if len(args.split(",")) != 2:
-    raise Exception("invalid argument for precompiles {0}".format(args))
+  acct = json.loads(arg)
 
-  address = arg.split(",")[0] #TODO validation
-  codefile = arg.split(",")[1]
+  if not 'address' in acct:
+    raise Exception("account must have an address")
 
-  try:
-    with open(codefile, 'rb') as f:
-      genesis['alloc'][address] = binascii.hexlify(f.read())
-  except Exception as e:
-    print(e)
-    sys.exit(1)
+  if not 'balance' in acct:
+    acct['balance'] = 0
+
+  if 'codefile' in acct:
+    try:
+      with open(codefile, 'rb') as f:
+        acct['code'] = binascii.hexlify(f.read())
+    except Exception as e:
+      print(e)
+      sys.exit(1)
+  elif not 'code' in acct:
+    acct['code'] = ''
+    
+  genesis['alloc'][acct['address']] = json.dumps({
+    'code': acct['code'],
+    'balance': acct['balance']
+  })
 
 print(genesis)
